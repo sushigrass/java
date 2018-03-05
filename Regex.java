9b1353c2695be64db85bcac2f5fb188769190a6b
sushigrass <johngillis707@gmail.com>
2018-03-04 20:58:14 -0400

    regex

---
+++ Regex.java
@@ -0,0 +1,25 @@
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "[a-zA-Z]\\w{7,29}"; //First char alphabetic, then 7-29 alphanumeric chars
+    public static final String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+    public static final String pattern = "(" + zeroTo255 + "\\.){3}" + zeroTo255; //IP addresses
+}
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
